3ef3111af5bdba33cc96c16906249f54745edac7
Asuki Kono <asukiaaa@gmail.com>
2021-02-10 12:54:43 +0900

    Update commend on readMultiple.ino

--- examples/readMultiple/readMultiple.ino
+++ examples/readMultiple/readMultiple.ino
@@ -6,35 +6,35 @@ int pinSlk = 4;
 HX711_asukiaaa::Reader reader(pinsDout, numPins, pinSlk);
 
 //---------------------------------------------------//
-// load cell SC301A 100kg
+// Load cell SC301A 100kg
 // https://akizukidenshi.com/catalog/g/gP-12036/
 //---------------------------------------------------//
 // #define LOAD_CELL_RATED_VOLT 0.002f
 // #define LOAD_CELL_RATED_GRAM 100000.0f
 
 //---------------------------------------------------//
-// load cell SC601 120kg
+// Load cell SC601 120kg
 // https://akizukidenshi.com/catalog/g/gP-12035/
 //---------------------------------------------------//
 #define LOAD_CELL_RATED_VOLT 0.002f
 #define LOAD_CELL_RATED_GRAM 120000.0f
 
 //---------------------------------------------------//
-// load cell SC133 20kg
+// Load cell SC133 20kg
 // https://akizukidenshi.com/catalog/g/gP-12034/
 //---------------------------------------------------//
 // #define LOAD_CELL_RATED_VOLT 0.002f
 // #define LOAD_CELL_RATED_GRAM 20000.0f
 
 //---------------------------------------------------//
-// load cell SC616C 500g
+// Load cell SC616C 500g
 // https://akizukidenshi.com/catalog/g/gP-12532/
 //---------------------------------------------------//
 // #define LOAD_CELL_RATED_VOLT 0.0007f
 // #define LOAD_CELL_RATED_GRAM 500.0f
 
 //---------------------------------------------------//
-// Resistor value for HX711 mofule sold on Akizuki
+// Resistors for HX711 module AE-HX711-SIP
 // https://akizukidenshi.com/catalog/g/gK-12370/
 //---------------------------------------------------//
 #define HX711_R1 20000.0
